bf27cb4a1de388fcd591a3f851b0a45e7379731b
Hilgard van Rooyen <hilgardvr@gmail.com>
2023-04-22 00:24:12 +0200

    part2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,54 +1,10 @@
+import Course from './Course'
+
 const Header = ({name}) => {
   return (
       <h1>{name}</h1>
   )
 }
-
-const CourseHeader = ({name}) => {
-  return (
-      <h3>{name}</h3>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part} {props.exercises}
-    </p>
-  )
-}
-
-const Content = ({props}) => {
-    return (
-      <div>
-        {props.parts.map(part => <Part key={part.id} part={part.name} exercises={part.exercises} />)}
-      </div>
-    )
-}
-
-const Total = ({props}) => {
-    const total = props.parts.reduce((acc, v) => acc + v.exercises, 0)
-    return (
-      <p>
-        <b>total of {total} exercises </b>
-      </p>
-    )
-}
-
-const Course = ({courses}) => {
-  return (
-      <div>
-        {courses.map(element => {
-          return <div key={element.id}>
-            <CourseHeader name={element.name} />
-            <Content props={element} />
-            <Total props={element} />
-          </div>
-        })}
-      </div>
-  )
-}
-
 
 const App = () => {
   const courses = [
