2d57520d2952aaed6a8e45bbbf054f6ee39f6cd9
Hilgard van Rooyen <hilgardvr@gmail.com>
2023-04-22 09:01:33 +0200

    part2.6

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+
+const Numbers = ({persons}) => {
+  return (
+    <>
+      {persons.map(person => 
+        <div>{person.name}</div>)}
+    </>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { 
+      id: 0,
+      name: 'Arto Hellas',
+    }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addName = (event) => {
+    event.preventDefault()
+    const person = {
+      id: persons.length,
+      name: newName,
+    }
+    setPersons(persons.concat(person))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input onChange={handleChange} value={newName}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <Numbers persons={persons} />
+    </div>
+  )
+}
+
+export default App
