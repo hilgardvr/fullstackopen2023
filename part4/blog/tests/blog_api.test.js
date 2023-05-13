0e1d6df730091805878c79344daef270605e7978
Hilgard van Rooyen <hilgardvr@gmail.com>
2023-05-13 12:26:19 +0200

    part4.8

--- part4/blog/tests/blog_api.test.js
+++ part4/blog/tests/blog_api.test.js
@@ -2,17 +2,42 @@ const mongoose = require('mongoose')
 const app = require('../app')
 const supertest = require('supertest')
 const api = supertest(app)
+const Blog = require('../models/blog')
+
+const initBlogs = [
+    {
+        "title": "Test Title",
+        "author": "John Galt",
+        "url": "www.johngalt.com",
+        "likes": 10,
+    },
+    {
+        "title": "Test Title1",
+        "author": "John Galt",
+        "url": "www.johngalt.com",
+        "likes": 10,
+    }
+]
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    const blogs = initBlogs.map(b => new Blog(b))
+    const promiseBlogs = blogs.map(b => b.save())
+    await Promise.all(promiseBlogs)
+})
 
 
 test('blogs are returned as json', async () => {
-    await api
-        .get('/api/blogs')
+    await api.get('/api/blogs')
         .expect(200)
         .expect('Content-Type', /application\/json/)
 })
 
+test('all blogs are returned', async () => {
+    const resp = await api.get('/api/blogs')
+    expect(resp.body).toHaveLength(2)
+})
+
 afterAll(async () => {
-    console.log('closing mongoose connection')
-    const closed = await mongoose.connection.close()
-    console.log(`closed connection`)
+    await mongoose.connection.close()
 })
