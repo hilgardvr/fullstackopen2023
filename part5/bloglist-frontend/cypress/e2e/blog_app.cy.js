6cb14c0c0c4c41b2be4a5f2207e4c3023bc93512
Hilgard van Rooyen <hilgard@onespark.co.za>
2023-06-10 09:37:38 +0200

    part5.22

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -3,20 +3,26 @@ import axios from 'axios'
 describe('blog app', () => {
 
   const initUsers = [
-      {
-          "username": "John",
-          "name": "John",
-          "password": "password123"
-      }, 
+    {
+      "username": "John",
+      "name": "John",
+      "password": "password123"
+    },
+    {
+      "username": "Peter",
+      "name": "Peter",
+      "password": "password123"
+    }
   ]
   const addUser = async () => {
     const response = await axios.post("http://localhost:3003/api/users", initUsers[0])
+    const response2 = await axios.post("http://localhost:3003/api/users", initUsers[1])
     console.log(response)
   }
 
   beforeEach(() => {
-    addUser()
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    addUser()
     cy.visit('http://localhost:3000')
   })
 
@@ -43,13 +49,13 @@ describe('blog app', () => {
     })
 
     describe('when logged in', () => {
-      beforeEach( () => {
+      beforeEach(() => {
         cy.get('.username').type(initUsers[0].username)
         cy.get('.password').type(initUsers[0].password)
         cy.get('.login').click()
       })
 
-      it('can create a new blog', () => {
+      it('can create, like and delete a new blog', () => {
         cy.contains('New blog').click()
         cy.get('.title').type('new title')
         cy.get('.author').type('new author')
@@ -59,9 +65,55 @@ describe('blog app', () => {
         cy.contains('new blog')
         cy.contains('new author')
       })
+
+      it('can like a blog', () => {
+        cy.contains('New blog').click()
+        cy.get('.title').type('new title')
+        cy.get('.author').type('new author')
+        cy.get('.url').type('new url')
+        cy.contains('Post').click()
+        cy.contains('Posted new blog: new title')
+        cy.contains('new blog')
+        cy.contains('new author')
+        cy.contains('show').click()
+        cy.contains('0')
+        cy.contains('like').click()
+        cy.contains('1')
+      })
+
+      it('can delete a blog', () => {
+        cy.contains('New blog').click()
+        cy.get('.title').type('new title')
+        cy.get('.author').type('new author')
+        cy.get('.url').type('new url')
+        cy.contains('Post').click()
+        cy.contains('Posted new blog: new title')
+        cy.contains('new blog')
+        cy.contains('new author')
+        cy.contains('show').click()
+        cy.contains('remove').click()
+        cy.contains('Deleted blog: new title')
+      })
+
+      it('non creator cannot delete a blog', () => {
+        cy.contains('New blog').click()
+        cy.get('.title').type('new title')
+        cy.get('.author').type('new author')
+        cy.get('.url').type('new url')
+        cy.contains('Post').click()
+        cy.contains('Posted new blog: new title')
+        cy.contains('new blog')
+        cy.contains('new author')
+        cy.contains('logout').click()
+        cy.get('.username').type(initUsers[1].username)
+        cy.get('.password').type(initUsers[1].password)
+        cy.get('.login').click()
+        cy.contains('show').click()
+        cy.contains('remove').should('not.exist')
+      })
     })
   })
 
- 
+
 
 })
