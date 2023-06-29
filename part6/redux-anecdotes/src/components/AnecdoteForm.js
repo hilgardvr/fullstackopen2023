c1dcee2f9cfccc0964581f7235aff42edf5c8f01
Hilgard van Rooyen <hilgard@onespark.co.za>
2023-06-29 07:59:39 +0200

    part6.14 wip

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,13 +1,24 @@
 import { useDispatch } from "react-redux"
 import { addAncedote } from "../reducers/anecdoteReducer"
 
+
+const getId = () => (100000 * Math.random()).toFixed(0)
+
+const asObject = (anecdote) => {
+  return {
+    content: anecdote,
+    id: getId(),
+    votes: 0
+  }
+}
+
 const AnecdoteForm = () => {
     const dispatch = useDispatch()
 
     const addNew = (event) => {
         event.preventDefault()
         const anecdote = event.target.anecdote.value
-        dispatch(addAncedote(anecdote))
+        dispatch(addAncedote(asObject(anecdote)))
     }
     return (
         <div>
