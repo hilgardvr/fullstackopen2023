b83a76659af59c52fe1fd17d8eeeae374dd148ef
Hilgard van Rooyen <hilgard@onespark.co.za>
2023-06-29 07:59:39 +0200

    part6.14 wip

--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -2,7 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App'
+import { addAncedote } from './reducers/anecdoteReducer'
 import store from './reducers/store'
+import getAll from './services/anecdotes'
+
+getAll().then(anecdotes => {
+  anecdotes.forEach(a => store.dispatch(addAncedote(a)))
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
